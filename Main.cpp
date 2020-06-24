52b66ef3d190757bdccf196cc9c0b09814d3f768
Michael Peterson <Mike_Jon_Peterson@gmail.com>
2020-06-24 18:27:12 -0500

    need to use a SetPlayer function insted of calling Game2.Players[n] = Player1

--- Main.cpp
+++ Main.cpp
@@ -5,25 +5,25 @@
 using std::cout;
 using std::endl;
 int main() {
-	Game Game1, Game2(2);
+	Game Game1, Game2();
 	Player Player1, Player2, Player3("Lizzy", 40);
-	cout << Player1.GetLife() << endl;
-	cout << Player1.GetName() << endl;
-	cout << Player2.GetLife() << endl;
-	cout << Player2.GetName() << endl;
 
 	Player1.SetName("Mike");
 	Player1.SetLife(40);
 	Player2.SetName("Murry");
 	Player2.SetLife(40);
 
-	cout << Player1.GetLife() << endl;
-	cout << Player1.GetName() << endl;
-	cout << Player2.GetLife() << endl;
-	cout << Player2.GetName() << endl;
+	Game2.Players[0] = Player1;
+	Game2.Players[1] = Player2;
+
+	cout << Player3.GetLife() << endl;
+	cout << Player3.GetName() << endl;
 
 	cout << Game1.Size() << endl;
-	cout << Game2.Size();
+	cout << Game2.Size() << endl;
 
+	Game2.Players[2] = Player3;
+
+	cout << Game2.Size();
 	return 0;
 }
